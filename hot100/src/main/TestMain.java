b7543fdc6dd044bfabdcfdb7871bd6fe98a0feea
Qiang-happiness <wangqihang2025@163.com>
2026-03-25 15:08:09 +0800

    删除链表中倒数第N个节点

--- hot100/src/main/TestMain.java
+++ hot100/src/main/TestMain.java
@@ -7,18 +7,23 @@ import java.util.List;
 
 public class TestMain {
     public static void main(String[] args) {
-        CycleLinkList2 cycleLinkList  = new CycleLinkList2();
+        NthFromEndRemove nthFromEndRemove = new NthFromEndRemove();
 
-        ListNode node0 = new ListNode(3);
+        ListNode node0 = new ListNode(1);
         ListNode node1 = new ListNode(2);
-        ListNode node2 = new ListNode(0);
-        ListNode node3 = new ListNode(-4);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
         node0.next = node1;
         node1.next = node2;
         node2.next = node3;
-        node3.next = node1;
+        node3.next = node4;
 
-        ListNode node = cycleLinkList.detectCycle2(null);
-        System.out.println(node);
+        ListNode node = nthFromEndRemove.removeNthFromEnd(node0, 2);
+
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
     }
 }
---
+++ hot100/src/questions/NthFromEndRemove.java
@@ -0,0 +1,31 @@
+package questions;
+
+import pojo.ListNode;
+
+import java.util.ArrayList;
+
+public class NthFromEndRemove {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
